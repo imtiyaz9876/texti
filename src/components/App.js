146558005c5d0f838de91b72222c975520d2e93e
Priyansh Kumar <priyansh_shiraj@outlook.com>
2022-09-01 14:13:41 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> app
+      </header>
+    </div>
+  );
+}
+
+export default App;
