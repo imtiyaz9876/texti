ee2380025a910f4129362919f8e6ad1ecb46dfc1
imtiyaz9876 <md.imtiyazalam9876@gmail.com>
2022-09-03 14:34:20 +0530

    commit me

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-
+import Navbar from './Navbar';
+import Container from './Container';
 import './css/App.css';
 
 function App() {
   return (
+    <>
+    <Navbar />
+    <Container />
+    
     <div className="App">
       <header className="App-header"> app
       </header>
+
+    
     </div>
+    </>
   );
 }
 
